d5d6f2743f72a05915b4ca6a95273d751cbba316
Edwin Agudelo <eagudelo@hotmail.com>
2020-07-22 15:44:27 -0500

    Version nueva con ajustes de herencia(cap 9)

--- ConsoleApplication1/SpreadSheet.cpp
+++ ConsoleApplication1/SpreadSheet.cpp
@@ -1,8 +1,8 @@
 #include "SpreadSheet.h"
 
-int SpreadSheet::sCounter;
+//int SpreadSheet::sCounter;
 
-SpreadSheet::SpreadSheet(const SpreadSheetApplication& theApp, int inWidth, int inHeigh) :
+/*SpreadSheet::SpreadSheet(const SpreadSheetApplication& theApp, int inWidth, int inHeigh) :
 	mWidth(inWidth < kMaxWidth ? inWidth : kMaxWidth), 
 	mHeight(inHeigh < kMaxHeight ? inHeigh : kMaxHeight),
 	mTheApp(theApp){
@@ -50,7 +50,7 @@ SpreadSheet::SpreadSheet(const SpreadSheet& src) : mTheApp(src.mTheApp) {
 	}
 	for (int i = 0; i < mWidth; i++)
 		for (int j = 0; j < mHeight; j++)
-			mCells[i][j] = src.mCells[i][j];*/
+			mCells[i][j] = src.mCells[i][j];*//*
 
 	// Esto es una mejora
 	mId = sCounter++;
@@ -78,7 +78,7 @@ SpreadSheet& SpreadSheet::operator=(const SpreadSheet& rhs) {
 	}
 	for (int i = 0; i < mWidth; i++)
 		for (int j = 0; j < mHeight; j++)
-			mCells[i][j] = rhs.mCells[i][j];*/
+			mCells[i][j] = rhs.mCells[i][j];*/ /*
 	copyFrom(rhs);
 
 	return *this;
@@ -105,3 +105,4 @@ int SpreadSheet::getId() {
 void SpreadSheetCell::setColor(Colors color) {
 	mColor = color;
 }
+*/
